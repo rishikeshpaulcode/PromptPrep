e3e985856364497e54f1e0e9643797a8f1b9164e
rishikeshpaulcode <rishikeshpaul.code@gmail.com>
2026-09-23 16:04:41 +0530

    create model functions

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,2 +1,16 @@
 # created and returns ai models
+from langchain_google_genai import ChatGoogleGenerativeAI
+from schemas import QuestionInternal
+from dotenv import load_dotenv
 
+load_dotenv()
+ 
+def get_base_model(temperature: float = 0.7) -> ChatGoogleGenerativeAI:
+    return ChatGoogleGenerativeAI(
+        model="gemini-3.5-flash",
+        temperature=temperature,
+        timeout=30
+    )
+
+def get_structured_model(temperature: float = 0.7) -> ChatGoogleGenerativeAI:
+    return get_base_model(temperature).with_structured_output(QuestionInternal)
