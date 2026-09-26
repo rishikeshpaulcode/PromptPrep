0ea6fc2522f1ec80005efcfab04c86d9b572d524
rishikeshpaulcode <rishikeshpaul.code@gmail.com>
2026-09-26 17:36:12 +0530

    modify return type

--- backend/core/services.py
+++ backend/core/services.py
@@ -1,12 +1,12 @@
 import asyncio
 import core.chains as chains
-from core.schemas import QuizBatch
+from core.schemas import QuestionInternal
 
 class AIService:
     def __init__(self):
         self.question_batch_chain = chains.get_question_batch_chain()
 
-    async def generate_question_batch(self, topic: str, difficulty: str, count: int = 5) -> QuizBatch:
+    async def generate_question_batch(self, topic: str, difficulty: str, count: int = 5) -> list[QuestionInternal]:
         question_batch = await self.question_batch_chain.ainvoke({
             "topic": topic,
             "difficulty": difficulty,
